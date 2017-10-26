64b647d25a65e2498eedb843a9b77fc77e4ed652
wynthorpe <simongtt@gmail.com>
2017-10-26 15:16:14 +0100

    Initial commit

---
+++ DurationTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DurationTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string startTime = "08:00 PM";
+            string endTime = "10:00 AM";
+            string now = DateTime.Now.ToShortDateString();
+            TimeSpan duration;
+
+            if (DateTime.Parse(startTime) > DateTime.Parse(endTime))
+            {
+                DateTime newStart = DateTime.Parse(now + " " + startTime);
+                DateTime newEnd = DateTime.Parse(now + " " + endTime).AddDays(1);
+                duration = (newStart - newEnd);
+            }
+            else
+            {
+                DateTime newStart = DateTime.Parse(startTime);
+                DateTime newEnd = DateTime.Parse(endTime);
+                duration = (newStart - newEnd);
+            }
+
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(Math.Abs(duration.TotalHours));
+            Console.ReadLine();
+        }
+    }
+}
